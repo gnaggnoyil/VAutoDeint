2258aa138ceb8e9f8d8d7662a70fab81276ad48b
gnaggnoyil <gnaggnoyil@gmail.com>
2013-05-29 21:37:04 +0800

    license issues

--- externalfilters/IsCombedTIVTCport.cpp
+++ externalfilters/IsCombedTIVTCport.cpp
@@ -1,20 +1,20 @@
 /*
-  IsCombedTVITCport.cpp: Copyright (C) 2013  Acfun (www dot acfun dot tv)
-
+  VAutoDeint.py: Copyright (C) 2013  Acfun (www dot acfun dot tv)
+  
   Author: Li Yonggang (gnaggnoyil at gmail dot com)
   
   This file is part of the VAutoDeint project.
-
+  
   This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 3 of the License, or (at your option) any later version.
-
+  modify it under the terms of the GNU General Public License as
+  published by the Free Software Foundation; either version 3 of
+  the License, or (at your option) any later version.
+  
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   General Public License for more details.
-
+  
   You should have received a copy of the GNU Lesser General Public
   License along with the author; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
